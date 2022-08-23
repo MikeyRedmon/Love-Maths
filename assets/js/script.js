39e1183e069b34e6697e16e097bbd46ee53ec764
MikeyRedmon <michaelredmond059@gmail.com>
2022-08-23 12:52:43 +0000

    add code to increment correct and incorrect answer score

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,8 +42,10 @@ function checkAnswer(){
 
     if (isCorrect){
         alert("Hey! You got it right! :D")
+        incrementScore();
     } else {
         alert(`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1])
@@ -66,12 +68,22 @@ function calculateCorrectAnswer(){
     }
 }
 
+/**
+ * Gets the current score from the DOm and increants it by one
+ */
 function incrementScore(){
 
-}
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 
+}
+/**
+ * Gets the current tally of incorrect scores from the DOm and increants it by one
+ */
 function incrementWrongAnswer(){
 
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2){
